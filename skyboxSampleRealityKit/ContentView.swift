301e0d0b20cd8f60c8a5d22f5f0e4dd1151e84ac
Joe Heck <heckj@mac.com>
2024-08-19 10:21:04 -0700

    Initial Commit

---
+++ skyboxSampleRealityKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  skyboxSampleRealityKit
+//
+//  Created by Joseph Heck on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ skyboxSampleRealityKit/skyboxSampleRealityKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  skyboxSampleRealityKitApp.swift
+//  skyboxSampleRealityKit
+//
+//  Created by Joseph Heck on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct skyboxSampleRealityKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
